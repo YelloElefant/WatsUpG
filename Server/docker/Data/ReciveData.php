92c3d1154ceb2a0e33076ea9a9bb4d2ba3940e9b
YelloElefant <alextrotternz@gmail.com>
2023-09-28 19:08:58 +1300

    removing the file writeing stuff
    Signed-off-by: YelloElefant <alextrotternz@gmail.com>

--- Server/docker/Data/ReciveData.php
+++ Server/docker/Data/ReciveData.php
@@ -1,9 +1,9 @@
 <?php
 echo "ReciveData.php";
 $data = $_POST['data'];
-$file = fopen("data.txt", "w") or die("Unable to open file!");
-fwrite($file, $data);
-fclose($file);
+// $file = fopen("data.txt", "w") or die("Unable to open file!");
+// fwrite($file, $data);
+// fclose($file);
 
 echo "Data Recived";
 echo $data;
