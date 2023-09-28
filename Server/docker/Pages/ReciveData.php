d3852ab1e0cc1d95a772cb86d46474e0a7d03608
YelloElefant <alextrotternz@gmail.com>
2023-09-28 23:54:12 +1300

    testing zAdd
    Signed-off-by: YelloElefant <alextrotternz@gmail.com>

--- Server/docker/Pages/ReciveData.php
+++ Server/docker/Pages/ReciveData.php
@@ -5,14 +5,17 @@ echo "Data: ";
 // $file = fopen("data.txt", "w") or die("Unable to open file!");
 // fwrite($file, $data);
 // fclose($file);
-$data = json_encode($_POST);
+$data = $_POST['data'];
+$hostName = $_POST['hostName'];
+
 echo $data;
 
 $redis = new Redis();
+
 $redis->connect('redis', 6379);
 if ($redis->ping()) {
    echo "PONGn";
 }
 
 
-?>
+$redis->zAdd($hostName, $data);
