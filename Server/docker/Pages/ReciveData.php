f004a8735a7fd40f24f533100ea1d0b455ad4c38
YelloElefant <alextrotternz@gmail.com>
2023-09-29 00:36:15 +1300

    setting up redis to save the json strings
    Signed-off-by: YelloElefant <alextrotternz@gmail.com>

--- Server/docker/Pages/ReciveData.php
+++ Server/docker/Pages/ReciveData.php
@@ -1,21 +1,23 @@
 <?php
+
+
+
 echo "ReciveData.php";
 echo "<br>";
 echo "Data: ";
 // $file = fopen("data.txt", "w") or die("Unable to open file!");
 // fwrite($file, $data);
 // fclose($file);
-$data = $_POST['data'];
 $hostName = $_POST['hostName'];
+$data = json_encode($_POST);
 
 echo $data;
 
 $redis = new Redis();
 
-$redis->connect('redis', 6379);
+$redis->connect('redisStack', 6379);
 if ($redis->ping()) {
    echo "PONGn";
 }
 
-
-$redis->zAdd($hostName, $data);
+$redis->set($hostName, $data);
