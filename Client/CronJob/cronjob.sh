54d05d8f0e29a44ca96de3dd1fbf985ea1eda318
YelloElefant <alextrotternz@gmail.com>
2023-10-12 17:40:49 +1300

    fixing cron job
    Signed-off-by: YelloElefant <alextrotternz@gmail.com>

--- Client/CronJob/cronjob.sh
+++ Client/CronJob/cronjob.sh
@@ -8,10 +8,10 @@ networkName='Trotter'
 #echo 'Trotter'
 
 # get private ipv4 address
-privateIpv4=$(ifconfig eth0 | grep 'inet' | xargs | cut -d' ' -f2)
+privateIpv4=$(ip addr show dev eth0 | grep 'inet' | xargs | cut -d' ' -f2 | cut -d'/' -f1)
 #echo $privateIpv4
 # get private ipv6 address
-privateIpv6=$(ifconfig eth0 | grep 'inet6' | xargs | cut -d' ' -f2)
+privateIpv6=$(ip addr show dev eth0 | grep 'inet6' | xargs | cut -d' ' -f2 | cut -d'/' -f1)
 #echo $privateIpv6
 # get cpu usage as a percentage with 2 decimal places
 cpu=$(top -bn1 | grep load | awk '{printf "%.2f", $(NF-2)}' | sed -e 's/\ *$//g')
@@ -24,11 +24,12 @@ upTime=$(uptime | awk '{print $3,$4}' | cut -d' ' -f1)
 #echo $upTime
 # get disk usage as a percentage
 #df -h | grep '/dev/sda1' | awk '{print $5}'
-currentTime=$(date | cut -d' ' -f5)
+currentTime=$(date | cut -d' ' -f4)
 
 
 #curl -X POST -d '{id="'"$hostName"'"&hostName="'"$hostName"'"&networkName="Trotter"&privateIpv4="'"$privateIpv4"'"&privateIpv6="'"$privateIpv6"'"&cpu="'"$cpu"'memory="'"$memory"'uptime="'"$upTime"'"&token="'"123456789"'"}' http://192.168.1.34:2525/ReciveData.php
-#echo 'token=123456789&id='$hostName'&hostName='$hostName'&networkName=Trotter&privateIpv4='$privateIpv4'&privateIpv6='$privateIpv6'&cpu='$cpu'&memory='$memory'&uptime='$upTime''
+echo 'time='$currentTime'&token=123456789&id='$hostName'&hostName='$hostName'&networkName=testing&privateIpv4='$privateIpv4'&privateIpv6='$privateIpv6'&cpu='$cpu'&memory='$memory'&upTime='$upTime'' http://192.168.1.34:2525/ReciveData.php > hello.txt
+echo updated
 curl -X POST \
 -d 'time='$currentTime'&token=123456789&id='$hostName'&hostName='$hostName'&networkName=testing&privateIpv4='$privateIpv4'&privateIpv6='$privateIpv6'&cpu='$cpu'&memory='$memory'&upTime='$upTime'' http://192.168.1.34:2525/ReciveData.php
  
