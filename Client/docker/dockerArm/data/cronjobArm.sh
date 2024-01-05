74fadfa54145119958afbb168aab9ffad9764174
Alexander Trotter <alextrotternz@gmail.com>
2024-01-05 22:33:54 +1300

    adding fail over for network interfaces

--- Client/docker/dockerArm/data/cronjobArm.sh
+++ Client/docker/dockerArm/data/cronjobArm.sh
@@ -8,7 +8,12 @@ hostName=$(hostname)
 #id=$hostName
 
 id=$(cat /data/id)
+defaultAdapter=$(ip route | grep -m1 default | cut -d ' ' -f5)
+logicalName=$(lshw -c network | grep $defaultAdapter -B 5 -A 5 | grep "logical name:" -m1 | awk '{$1=$1};1' | cut -d ' ' -f3)
+defaultAdapterProtocol=$(lshw -class network | awk '/description/ {via_line=$0} /'$defaultAdapter'/ {print via_line}' | awk '{$1=$1};1' | cut -d ' ' -f2)
 
+echo $defaultAdapter
+echo $defaultAdapterProtocol
 echo $id
 
 
@@ -17,10 +22,10 @@ networkName='Trotter'
 #echo 'Trotter'
 
 # get private ipv4 address
-privateIpv4=$(ip addr show dev eth0 | grep 'inet' | xargs | cut -d' ' -f2 | cut -d'/' -f1)
+privateIpv4=$(ip addr show dev $defaultAdapter | grep 'inet' | xargs | cut -d' ' -f2 | cut -d'/' -f1)
 echo $privateIpv4
 # get private ipv6 address
-privateIpv6=$(ip addr show dev eth0 | grep 'inet6' | xargs | cut -d' ' -f2 | cut -d'/' -f1)
+privateIpv6=$(ip addr show dev $defaultAdapter | grep 'inet6' | xargs | cut -d' ' -f2 | cut -d'/' -f1)
 echo $privateIpv6
 # get cpu usage as a percentage with 2 decimal places
 cpu=$(top -bn1 | grep load | awk '{printf "%.2f", $(NF-2)}' | sed -e 's/\ *$//g')
@@ -39,7 +44,7 @@ currentTime=$(date | cut -d' ' -f5)
 #curl -X POST -d '{id="'"$hostName"'"&hostName="'"$hostName"'"&networkName="Trotter"&privateIpv4="'"$privateIpv4"'"&privateIpv6="'"$privateIpv6"'"&cpu="'"$cpu"'memory="'"$memory"'uptime="'"$upTime"'"&token="'"123456789"'"}' http://192.168.1.34:2525/ReciveData.php
 #echo 'time='$currentTime'&token=123456789&id='$hostName'&hostName='$hostName'&networkName=testing&privateIpv4='$privateIpv4'&privateIpv6='$privateIpv6'&cpu='$cpu'&memory='$memory'&upTime='$upTime'' http://192.168.1.34:2525/ReciveData.php > hello.txt
 #echo updated
-response=$(curl -X POST -d 'time='$currentTime'&token=123456789&id='$id'&hostName='$hostName'&networkName=testing&privateIpv4='$privateIpv4'&privateIpv6='$privateIpv6'&cpu='$cpu'&memory='$memory'&upTime='$upTime'' http://192.168.1.34:2525/ReciveData.php)
+response=$(curl -X POST -d 'time='$currentTime'&token=123456789&id='$id'&hostName='$hostName'&networkName=testing&privateIpv4='$privateIpv4'&privateIpv6='$privateIpv6'&cpu='$cpu'&memory='$memory'&upTime='$upTime'&adapter='$defaultAdapter'&adapterProtocol='$defaultAdapterProtocol'' http://192.168.1.34:2525/ReciveData.php)
 
 echo $response 
 newId=$(echo $response | grep 'id' | cut -d'=' -f2)
