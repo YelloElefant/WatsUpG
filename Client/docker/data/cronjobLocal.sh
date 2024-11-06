22722e2f1e2493ed2ff90d911d36ec93850a72d1
YelloElefant <alextrotternz@gmail.com>
2024-11-06 03:02:22 +0000

    client data gathering and sending is fully dynamic so can add more data easily
    Signed-off-by: YelloElefant <alextrotternz@gmail.com>

--- Client/docker/data/cronjobLocal.sh
+++ Client/docker/data/cronjobLocal.sh
@@ -9,7 +9,7 @@ construct_data() {
     data+="&$key=${clientData[$key]}"
   done
 
-  # remove the first character
+# remove the first character
   data=${data:1}
   echo "$data"
 }
@@ -20,22 +20,22 @@ construct_data() {
 # fi
 
 # Get host name
+hostName=$(hostname)
 declare -A clientData
 
 # Get id from id file
-clientData["hostName"]=$(hostname)
-clientData["id"]=$(cat /data/id)
-clientData["serverPath"]=$(cat /data/serverPath)
-clientData["adapter"]=$(ip route | grep -m1 default | cut -d ' ' -f5)
-clientData["logicalName"]=$(lshw -c network | grep ${clientData["adapter"]} -B 5 -A 5 | grep "logical name:" -m1 | awk '{$1=$1};1' | cut -d ' ' -f3)
-clientData["adapterProtocol"]=$(lshw -class network | awk '/description/ {via_line=$0} /'${clientData["adapter"]}'/ {print via_line}' | awk '{$1=$1};1' | cut -d ' ' -f2)
+clientData["id"]='test'
+clientData["serverPath"]=192.168.1.29
+clientData["defaultAdapter"]=$(ip route | grep -m1 default | cut -d ' ' -f5)
+clientData["logicalName"]=$(lshw -c network | grep ${clientData["defaultAdapter"]} -B 5 -A 5 | grep "logical name:" -m1 | awk '{$1=$1};1' | cut -d ' ' -f3)
+clientData["defaultAdapterProtocol"]=$(lshw -class network | awk '/description/ {via_line=$0} /'${clientData["defaultAdapter"]}'/ {print via_line}' | awk '{$1=$1};1' | cut -d ' ' -f2)
 clientData["networkName"]='None'
 routerIp=$(ip route | grep default | cut -d' ' -f3)
 if [ ! -z $(dig -x $routerIp +short) ]; then
   clientData["networkName"]=$(dig -x $routerIp +short | cut -d'.' -f1)
 fi
-clientData["privateIpv4"]=$(ip addr show dev ${clientData["adapter"]} | grep 'inet' | xargs | cut -d' ' -f2 | cut -d'/' -f1)
-clientData["privateIpv6"]=$(ip addr show dev ${clientData["adapter"]} | grep 'inet6' | xargs | cut -d' ' -f2 | cut -d'/' -f1)
+clientData["privateIpv4"]=$(ip addr show dev ${clientData["defaultAdapter"]} | grep 'inet' | xargs | cut -d' ' -f2 | cut -d'/' -f1)
+clientData["privateIpv6"]=$(ip addr show dev ${clientData["defaultAdapter"]} | grep 'inet6' | xargs | cut -d' ' -f2 | cut -d'/' -f1)
 clientData["cpu"]=$(top -bn1 | grep load | awk '{printf "%.2f", $(NF-2)}' | sed -e 's/\ *$//g')
 clientData["memory"]=$(free | grep Mem | awk '{printf "%.2f", $3/$2 * 100.0}')
 clientData["upTime"]=$(uptime | awk '{print $3,$4}' | cut -d' ' -f1)
@@ -43,9 +43,6 @@ clientData["currentTime"]=$(date +%T)
 clientData["os"]=$(cat /etc/os-release | grep 'PRETTY_NAME' | cut -d'=' -f2 | sed 's/"//g' | cut -d' ' -f1)
 clientData["diskUsage"]=$(df -h | grep '/dev/sda1' | awk '{print $5}')
 clientData["osVersion"]=$(cat /etc/os-release | grep 'PRETTY_NAME' | cut -d'=' -f2 | sed 's/"//g' | cut -d' ' -f2)
-clientData["token"]=123456789
-clientData["type"]="client"
-clientData["time"]=$(date +%T)
 
 # Construct data string
 data=$(construct_data)
@@ -63,12 +60,12 @@ if [ $newId != 'known' ]; then
   echo $newId > /data/id
 fi
 
-if [ $1 -eq 59 ]; then
-  exit 0
-fi
+# if [ $1 -eq 59 ]; then
+#   exit 0
+# fi
 
-echo "Script called. Count: $1"
+# echo "Script called. Count: $1"
 
-sleep 1
+# sleep 1
 
-/cronjob.sh $(( $1 + 1 ))
+# /cronjob.sh $(( $1 + 1 ))
--- Client/docker/data/test.sh
+++ Client/docker/data/test.sh
@@ -1,13 +1,72 @@
 #!/bin/bash
 
-networkName='None'
-# get the ip of the router in curretn subenet
-routerIp=$(ip route | grep default | cut -d' ' -f3)
-# reverse dns lookup with dig command and check if it is not empty
-if [ ! -z $(dig -x $routerIp +short) ]
-then
-  networkName=$(dig -x $routerIp +short | cut -d'.' -f1)
-fi
+# Function to dynamically add new data types
+construct_data() {
+  local data=""
+  
+  # Add additional data types here
+  for key in "${!clientData[@]}"; do
+    echo $key : ${clientData[$key]}
+    data+="&$key=${clientData[$key]}"
+  done
 
-echo $routerIp
-echo $networkName
+  # remove the first character
+  data=${data:1}
+  echo "$data"
+}
+
+# echo $1
+# if [[ $1 == *"0"* ]]; then
+#   /endPointQuery.sh
+# fi
+
+# Get host name
+hostName=$(hostname)
+declare -A clientData
+
+# Get id from id file
+clientData["id"]="test"
+clientData["serverPath"]=192.168.1.29
+clientData["defaultAdapter"]=$(ip route | grep -m1 default | cut -d ' ' -f5)
+clientData["logicalName"]=$(lshw -c network | grep ${clientData["defaultAdapter"]} -B 5 -A 5 | grep "logical name:" -m1 | awk '{$1=$1};1' | cut -d ' ' -f3)
+clientData["defaultAdapterProtocol"]=$(lshw -class network | awk '/description/ {via_line=$0} /'${clientData["defaultAdapter"]}'/ {print via_line}' | awk '{$1=$1};1' | cut -d ' ' -f2)
+clientData["networkName"]='None'
+routerIp=$(ip route | grep default | cut -d' ' -f3)
+if [ ! -z $(dig -x $routerIp +short) ]; then
+  clientData["networkName"]=$(dig -x $routerIp +short | cut -d'.' -f1)
+fi
+clientData["privateIpv4"]=$(ip addr show dev ${clientData["defaultAdapter"]} | grep 'inet' | xargs | cut -d' ' -f2 | cut -d'/' -f1)
+clientData["privateIpv6"]=$(ip addr show dev ${clientData["defaultAdapter"]} | grep 'inet6' | xargs | cut -d' ' -f2 | cut -d'/' -f1)
+clientData["cpu"]=$(top -bn1 | grep load | awk '{printf "%.2f", $(NF-2)}' | sed -e 's/\ *$//g')
+clientData["memory"]=$(free | grep Mem | awk '{printf "%.2f", $3/$2 * 100.0}')
+clientData["upTime"]=$(uptime | awk '{print $3,$4}' | cut -d' ' -f1)
+clientData["currentTime"]=$(date +%T)
+clientData["os"]=$(cat /etc/os-release | grep 'PRETTY_NAME' | cut -d'=' -f2 | sed 's/"//g' | cut -d' ' -f1)
+clientData["diskUsage"]=$(df -h | grep '/dev/sda1' | awk '{print $5}')
+clientData["osVersion"]=$(cat /etc/os-release | grep 'PRETTY_NAME' | cut -d'=' -f2 | sed 's/"//g' | cut -d' ' -f2)
+
+# Construct data string
+data=$(construct_data)
+
+# Send data to server
+# response=$(curl -X POST -d "$data" http://$serverPath:2525/Api/PostClientData.php)
+
+echo $data
+
+# echo $response 
+# newId=$(echo $response | grep 'id' | cut -d'=' -f2)
+# echo $newId
+
+# if [ $newId != 'known' ]; then
+#   echo $newId > /data/id
+# fi
+
+# if [ $1 -eq 59 ]; then
+#   exit 0
+# fi
+
+# echo "Script called. Count: $1"
+
+# sleep 1
+
+# /cronjob.sh $(( $1 + 1 ))
