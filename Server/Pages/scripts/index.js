9f1c54b0d56395c188aaa258348c7ae4931aad1b
YelloElefant <alextrotternz@gmail.com>
2025-01-13 15:16:19 +1300

    adding a colapasble sidebar/nav bar
    Signed-off-by: YelloElefant <alextrotternz@gmail.com>

--- Server/Pages/scripts/index.js
+++ Server/Pages/scripts/index.js
@@ -16,7 +16,32 @@ let navToHighlight = document.getElementById(currentPath + "Link");
 navToHighlight.style.backgroundColor = "#171b20";
 navToHighlight.style.borderRadius = "0.375rem";
 
+let colapsed = false;
+document.getElementById("sidebarColapse").addEventListener("click",
+   () => {
+      let sidebarWrapper = document.getElementById("sidebarWrapper");
+      let words = document.getElementsByClassName("linkWord");
+      let titleParts = document.getElementById("sidebarTitle").children;
 
+      let collapeIcon = document.getElementById("collapeIcon");
+      let attributes = !colapsed ? ["none", "auto", "rotate(180)"] : ["inline", "300px", "rotate(0)"];
+      for (let i = 0; i < words.length; i++) {
+         words[i].style.display = attributes[0];
+      }
+
+      for (let i = 1; i < titleParts.length; i += 2) {
+         titleParts[i].style.display = attributes[0];
+      }
+
+      sidebarWrapper.style.width = attributes[1];
+
+      collapeIcon.setAttribute(
+         "transform",
+         attributes[2]
+      );
+      colapsed = colapsed ? false : true;
+   }
+);
 
 
 // let clientCard = document.getElementsByClassName("clientCard")[0];
